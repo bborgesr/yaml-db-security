e518d505008a501172f670e97dda62ab776aff64
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2016-07-26 15:32:51 +0100

    simplified server

--- app/server.R
+++ app/server.R
@@ -5,22 +5,17 @@ library(yaml)
 
 credentials <- yaml.load_file("../db.yml")
 
+pool <- NULL
+try( pool <- dbPool(
+  drv = RMySQL::MySQL(),
+  dbname = credentials$dbname,
+  host = credentials$host,
+  username = credentials$username,
+  password = credentials$password
+))
+
 function(input, output) {
-  pool <- NULL
-  
-  createPool <- function() {
-    try( pool <<- dbPool(
-      drv = RMySQL::MySQL(),
-      dbname = credentials$dbname,
-      host = credentials$host,
-      username = credentials$username,
-      password = credentials$password
-    ))
-  }
-  
-  # show pool
   output$dataInfo <- renderPrint({
-    createPool()
     if (!is.null(pool)) dbGetInfo(pool)
     else "You do NOT have access to this database"
   })
